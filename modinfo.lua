9ba50fba4a5dbd16da4a23ca19d99554da352e40
splorange12 <trentw25@frontier.com>
2023-12-29 02:11:00 -0500

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,53 @@
+name = "splorange's QOL"
+--The name of your mod
+description = "Simple QOL mod that aims to remove small annoyances"
+--The description that shows when you are selecting the mod from the list
+author = "splorange"
+--Your name!
+version = "1.0"
+
+forumthread = ""
+
+icon_atlas = "modicon.xml"
+
+icon = "modicon.tex"
+
+dst_compatible = true
+forge_compatible = true
+gorge_compatible = true
+
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+all_clients_require_mod = false
+server_only_mod = true
+client_only_mod = false
+
+api_version = 10
+--This is the version of the game's API and should stay the same for the most part
+
+configuration_options =
+{
+	{
+	    name = "BELLED_BEEFALO_POOP",
+	    label = "No Belled Beefalo Poop",
+        options = {
+	        {description = "Yes", data = true},
+	        {description = "No", data = false},
+	    },
+	    default = true,
+	    hover = "Removes Belled Beefalo's Ability to Poop"
+	},
+
+	{
+	    name = "GUANO_LOOT",
+	    label = "No Batilisk Guano Loot",
+        options = {
+	        {description = "Yes", data = true},
+	        {description = "No", data = false},
+	    },
+	    default = true,
+	    hover = "Removes Guano from the Batilisk's Loot Pool"
+	}
+}
---
+++ modmain.lua
@@ -0,0 +1,62 @@
+
+local BELLED_BEEFALO_POOP = GetModConfigData("BELLED_BEEFALO_POOP")
+local GUANO_LOOT = GetModConfigData("GUANO_LOOT")
+
+local function GetUpvalue(func, name)
+	local debug = GLOBAL.debug
+	local i = 1
+	while true do
+		local n, v = debug.getupvalue(func, i)
+		if not n then
+			return nil, nil
+		end
+		if n == name then
+			return v, i
+		end
+		i = i + 1
+	end
+end
+
+local function SetUpvalue(func, ind, value)
+	local debug = GLOBAL.debug
+	debug.setupvalue(func, ind, value)
+end
+
+---No Belled Beefalo Poop-----------------------------------
+
+local function HackBeefaloPoop()
+	local beefalo_fn = GLOBAL.Prefabs["beefalo"].fn
+
+	local CanSpawnPoop, CanSpawnPoop_index = GetUpvalue(beefalo_fn, "CanSpawnPoop")
+
+    local old_CanSpawnPoop = CanSpawnPoop
+    local new_CanSpawnPoop = function(inst)
+        if inst.components.hitchable and not inst.components.hitchable.canbehitched then return false end
+        if inst.components.follower:GetLeader() ~= nil then return false end
+        return inst.components.rideable == nil or not inst.components.rideable:IsBeingRidden()
+    end
+
+	SetUpvalue(beefalo_fn, CanSpawnPoop_index, new_CanSpawnPoop)
+end
+
+if BELLED_BEEFALO_POOP then
+	AddPrefabPostInit("world", HackBeefaloPoop)
+end
+
+------------------------------------------------------------
+
+---No Guano Drops-------------------------------------------
+
+local function RemoveGuanoFromBat()
+	GLOBAL.SetSharedLootTable("bat",
+	{
+		{"batwing",    0.25},
+		{"monstermeat",0.10},
+	})
+end
+
+if GUANO_LOOT then
+	AddPrefabPostInit("world", RemoveGuanoFromBat)
+end
+
+------------------------------------------------------------
